022c3ced714b609f792a6ea0912ba88184f072df
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-06 00:06:22 -0500

    Move the Rigidbody.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine.SceneManagement;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
