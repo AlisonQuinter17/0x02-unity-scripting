75dac1c1af1474bb342e396dc77b8eb92c2af6b7
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-05 23:54:10 -0500

    Move the Rigidbody.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 2000f;
+
+    // Fixed Update is called once per frame
+    public void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
