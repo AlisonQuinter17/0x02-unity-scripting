2c614a78053da2c96562ec4df44c249246c0d9d3
AlisonQuinter17 <alisondanellaquintero@hotmail.com>
2021-05-06 16:40:10 -0500

    Move the Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,17 +3,11 @@
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset;
+    public Vector3 offSet;
 
-    void Start () 
+    // FixedUpdate
+    public void FixedUpdate()
     {
-        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + offSet ;
     }
 }
